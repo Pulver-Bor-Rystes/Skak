691feeca16909d3914e8656f73751ffa1f89f102
Benjamin Jensen  Desktop <BenjaminByvej@gmail.com>
2021-08-31 22:29:47 +0200

    More testing..

---
+++ web/js/svelte/pvp_match.svelte
@@ -0,0 +1,23 @@
+<script>
+    import * as data from './fen.json';
+    const fen = data.fen;
+    console.log(fen); // output 'testing'
+
+
+    class Pawn {
+        
+        constructor(x,y) {
+            this.x = x
+            this.y = y
+        }
+    }
+</script>
+
+<p style="color:red">
+    {fen}
+</p>
+
+<!-- Vis billede i div loadet fra script som objekt --> 
+<!-- <div on:mousemove={handleMousemove}> --> 
+<!--  --> 
+<!--  --> 
