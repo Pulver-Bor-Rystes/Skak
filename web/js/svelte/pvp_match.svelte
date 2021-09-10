614a415d895a25b4049eef4abffd015b59d47552
Benjamin Jensen  Desktop <BenjaminByvej@gmail.com>
2021-09-10 23:30:40 +0200

    Grid WIP

--- web/js/svelte/pvp_match.svelte
+++ web/js/svelte/pvp_match.svelte
@@ -1,10 +1,25 @@
 <script>
     import * as data from './fen.json';
     const fen = data.fen;
-    console.log(fen); // output 'testing'
 
+    var l = function () {
+        console.log.apply(console, arguments);
+    }
     
+    l(fen); // output 'testing'
 
+    const bn ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/bn.png"
+    const bb ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/bb.png"
+    const br ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/br.png"
+    const bq ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/bq.png"
+    const bk ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/bk.png"
+    const bp ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/bp.png"
+    const wn ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/wn.png"
+    const wb ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/wb.png"
+    const wr ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/wr.png"
+    const wq ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/wq.png"
+    const wk ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/wk.png"
+    const wp ="https://images.chesscomfiles.com/chess-themes/pieces/wood/150/wp.png"
 
     class Pawn {
         
@@ -22,28 +37,40 @@
     var tooSmall = 300
     var smallestEvent = -Infinity
     var biggestEvent = Infinity
-    function zoomDownActivate(event) {
+
+    const zoomDownActivate = event => {
         zoomDown = true
         downCoord = event.y - (boardPlacement.top+boardSize)
     }
     
-    function zoomDownDeactivate() {
+    const zoomDownDeactivate = () => {
         if(zoomDown) zoomDown = false
     }
 
-    function changeSize(event) {
+    const changeSize = event => {
         if(zoomDown) {
-            console.log(event.y + ", " + smallestEvent)
+            l(event.y + ", " + smallestEvent)
             if(event.y > boardPlacement.top + tooSmall && event.y < boardPlacement.top + tooBig) {
                 boardSize = event.y - boardPlacement.top - downCoord
             }
         }
 
     }
-    
-
 
     
+    //Only works onclick not onload or immediately?
+    const peewee = () => {
+        var pieceDivs = document.querySelectorAll(".chessPiece")
+        l(pieceDivs)
+        for (let i = 0; i < pieceDivs.length; i++) {
+            const piece = pieceDivs[i]
+            piece.onclick = () => {
+                console.log("pee")
+            }
+        }
+    }
+        
+
 </script>
 
 
@@ -52,15 +79,55 @@
 <!-- <div on:mousemove={handleMousemove}> --> 
 <!--  --> 
 <!--  --> 
-<window on:mousemove={changeSize} on:mouseup={zoomDownDeactivate}>
+<window on:mousemove={changeSize} on:mouseup={zoomDownDeactivate} onload={peewee}>
 
-<div class="parent" >
+<div class="parent">
     <img id="pee" class="image1" src="https://images.chesscomfiles.com/chess-themes/boards/dash/150.png" draggable="false" alt="Board" style="width: {boardSize}px;" on:load={
         boardPlacement = document.getElementById("pee").getBoundingClientRect()
     }>
 
-    <img class="image2" src="https://via.placeholder.com/50" draggable="false" alt="Piece" style="width: {boardSize/8}px;">
-    <img on:mousedown={zoomDownActivate}  class="image3" src="https://via.placeholder.com/50" alt="Scroll" style="width: {boardSize/15}px;" draggable="false">
+
+    <div class="chessBoard" style="grid-template-columns: {boardSize/8}px {boardSize/8}px {boardSize/8}px {boardSize/8}px {boardSize/8}px {boardSize/8}px {boardSize/8}px {boardSize/8}px;">
+        <img class="chessPiece" src="{br}" alt="1" draggable="false">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+        <img class="chessPiece" alt="2">
+ 
+    </div>
+    <img on:mousedown={zoomDownActivate}  class="zoomer" src="https://via.placeholder.com/50" alt="Scroll" style="width: {boardSize/15}px;" draggable="false">
+    <img on:mousedown={peewee}  class="zoomer" src="https://via.placeholder.com/50" alt="Scroll" style="width: {boardSize/15}px;" draggable="false">
   </div>
   <canvas id="visuals">
 
@@ -68,6 +135,7 @@
 </window>
 
   <style>
+    
     .parent {
         position:absolute;
         top:100px;
@@ -81,13 +149,18 @@
         border: 1px red solid;
         
     }
-    .image2 {
+    .chessBoard {
+        display: grid;
         position: absolute;
         top: 0;
         left: 0;
         border: 1px green solid;
     }
-    .image3 {
+    .chessPiece {
+        display: relative;
+        border: 1px green solid;
+    }
+    .zoomer {
         position: relative;
         top: 0;
         left: 0;
