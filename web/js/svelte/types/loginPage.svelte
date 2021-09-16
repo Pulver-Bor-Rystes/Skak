91bef45fc60e12a03c711c1d429cc26367cf4f0e
Peter Hemmingsen <petervesthem@gmail.com>
2021-09-16 15:34:47 +0200

    login_page

---
+++ web/js/svelte/types/loginPage.svelte
@@ -0,0 +1,43 @@
+
+<body class="body"></body>
+<h1 class="headline">Bruger-login</h1>
+
+<form action="" class="inputs">
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email"><br><br>
+  <label for="password">Password:</label>
+  <input type="text" id="password" name="password"><br><br>
+  <input onclick="" type="submit" value="🢂" class="button">
+</form>
+
+<style>
+	
+	.body {
+  background-color: #383941;
+}
+    .headline {
+        top: 25px;
+				left: 270px;
+				position: absolute;
+				color: white;
+    }
+		.inputs {
+			position: absolute;
+			top: 115px;
+			left: 270px;
+			background: #383941;
+			color: white;
+		}
+	
+	.button {
+border-radius: 100px;
+		border: 2px solid #383941;
+	}
+	
+
+	
+</style>
+
+<script>
+
+</script>
