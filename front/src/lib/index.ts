1f3b00d3dc97e189789260d789b3975b0b6eedad
Rasmus Foldberg Birkebæk <rasmusfoldberg@gmail.com>
2025-05-23 16:37:26 +0200

    commiter alt før jeg auditer fix :))

--- front/src/lib/index.ts
+++ front/src/lib/index.ts
@@ -1,1 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import init, { greet } from "chess_machine_lib";
+
+
+export async function greet_test() {
+    await init();
+    console.log(greet("World"));
+}
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
     import Button from '$lib/components/button.svelte';
     import Login from '$lib/components/debug/login.svelte';
+
+    // import { greet } from 'chess_machine_lib';
+    import { greet_test } from '$lib';
     
     import { active_players, engines, username } from '$lib/state';
     import { socket } from '$lib/ws';
 
     function new_game(engine: string) {
         $socket.send("newgame", {
-            
+            username: engine,
+            timeformat: "lalal",
         })
+    }
+
+    function click() {
+        $socket.send("getstate", {})
+        greet_test();
     }
 </script>
 
-<div class="m-5 shadow border-rounded bg-[#ffffff2d] w-128 h-72 rounded-xl grid" style="grid-template-columns: 30% auto;">
+<div class="m-5 shadow border-rounded bg-[#ffffff2d] w-128 rounded-xl grid" style="grid-template-columns: 30% auto;">
     <div class="m-1">
         <Login/>
 
@@ -24,12 +33,24 @@
                 {/if}
             {/each}
         </ul>
+
+        <br>
+
         <h3 class="m-1"> Bots </h3>
         {#each $engines as engine}
             <Button fn={ () => { new_game(engine) } }> {engine} </Button>
         {/each}
+
+        <br><br>
+        <h3 class="m-1"> Dev Tools </h3>
+
+        <Button fn={click}> Get state </Button>
     </div>
+
+
     <div>
         
     </div>
+
+
 </div>
